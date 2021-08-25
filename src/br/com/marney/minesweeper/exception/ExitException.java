1809e381d928bfbc62352547fd6dd0424b3886f4
marneydiniz <86838602+marneydiniz@users.noreply.github.com>
2021-08-24 22:05:13 -0300

    Delete src/br/com/marney/minesweeper/exception directory

--- src/br/com/marney/minesweeper/exception/ExitException.java
+++
@@ -1,7 +0,0 @@
-package br.com.marney.minesweeper.exception;
-
-public class ExitException extends RuntimeException{
-
-	private static final long serialVersionUID = 1;
-
-}
--- src/br/com/marney/minesweeper/exception/ExplosionException.java
+++
@@ -1,7 +0,0 @@
-package br.com.marney.minesweeper.exception;
-
-public class ExplosionException extends RuntimeException {
-
-	private static final long serialVersionUID = 1L;
-
-}
